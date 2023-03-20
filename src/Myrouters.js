a0bc87e3251d8a1e12e5380f6c281cfa01cea992
Wala <rohan.jayesh.wala@accenture.com>
2023-03-20 09:29:21 +0530

    for deployment

--- src/Myrouters.js
+++ src/Myrouters.js
@@ -5,7 +5,7 @@ import Profile from "./Profile";
 
 export default function Myrouter() {
 	return (
-		<BrowserRouter>
+		<BrowserRouter basename="/ReactSample">
 			<Home></Home>
 			<Routes>
 				<Route path="/" element={<Home></Home>}></Route>
